1e060d0ee3b83f3fa09d52e88e3c94c6a4904f7c
Alexander Mescheryakov <sibsfinx@ya.ru>
2020-03-25 19:27:23 +0300

    init

---
+++ config/keys.js
@@ -0,0 +1,24 @@
+(function() {
+  var keys;
+
+  keys = {
+    s3: {
+      production: {
+        region: 'eu-west-2',
+        key: '',
+        secret: '',
+        bucket: ''
+      },
+      staging: {
+        region: 'eu-west-2',
+        key: '',
+        secret: '',
+        bucket: ''
+      }
+
+    }
+  };
+
+  module.exports = keys;
+
+}).call(this);
